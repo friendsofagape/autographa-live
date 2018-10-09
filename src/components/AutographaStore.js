8af901d5188d9e276d45951b4c57c64a1775fb64
Sandeep Kumar <sandeepkumar0486@gmail.com>
2018-10-09 17:30:38 +0530

    add paratext username and password

--- src/components/AutographaStore.js
+++ src/components/AutographaStore.js
@@ -62,6 +62,8 @@ export class AutographaStore {
   @observable incompleteVerse = {}
   @observable multipleSpaces = {}
   @observable setDiff = false
+  @observable username = ""
+  @observable password = ""
 
 
 
