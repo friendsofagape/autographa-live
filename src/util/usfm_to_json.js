9bc8245f8b01d8023b06934fa51c6371dbff4293
vipinpaul <vipin.paul@bridgeconn.com>
2020-02-24 11:23:41 +0530

    M tag with line break preserved on import

--- src/util/usfm_to_json.js
+++ src/util/usfm_to_json.js
@@ -42,7 +42,6 @@ module.exports = {
                     return callback(new Error(`${fileName(options.usfmFile)} ${AutographaStore.currentTrans["usfm-bookid-missing"]}`));
 
             validLineCount++;
-            line = line.trim();
             var splitLine = line.split(/ +/);
             if (!line) {
                 validLineCount--;
@@ -114,14 +113,22 @@ module.exports = {
                 //Do nothing for section headers now.
             } else if (splitLine.length === 1) {
                 // Do nothing here for now.
-            } else if (splitLine[0].startsWith('\\m')) {
-                // Do nothing here for now
+            } else if (splitLine[0].match(new RegExp(/\\m$/gm))) {
+                let cleanedStr = replaceMarkers(line);
+                cleanedStr = "\n" + cleanedStr;
+                book.chapters[c - 1].verses[vnum - 1].verse += ((cleanedStr.length === 0 ? '' : ' ') + cleanedStr);
             } else if (splitLine[0].startsWith('\\r')) {
                 // Do nothing here for now.
             } else if (c > 0 && vnum > 0) {
+                var qflag = false;
+                if(line.match(new RegExp(/[\\q\n]/g))){
+                    qflag = true
+                }
                 let cleanedStr = replaceMarkers(line);
-                book.chapters[c - 1].verses[v - 1].verse += ((cleanedStr.length === 0 ? '' : ' ') + cleanedStr);
-
+                if(qflag === false){
+                    cleanedStr = "\n" + cleanedStr;
+                }
+                book.chapters[c - 1].verses[vnum - 1].verse += ((cleanedStr.length === 0 ? '' : ' ') + cleanedStr);
             }
         });
 
